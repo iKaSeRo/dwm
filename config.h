03c737ed7011fc2b9d836a08dc6d3b57f08ffe99
iKaSeRo <A7med24.channel@gmail.com>
2020-12-02 16:37:28 +0300

    Changed Color to Green

--- config.h
+++ config.h
@@ -6,13 +6,13 @@ static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "MesloLGS NF:size=12", "fontawesome:size=12"};
+static const char *fonts[]          = { "MesloLGS NF:size=12", "fontawesome:size=12, JoyPixels:size=12"};
 static const char dmenufont[]       = "MesloLGS NF:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#e81e43";
+static const char col_cyan[]        = "#06a284";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
